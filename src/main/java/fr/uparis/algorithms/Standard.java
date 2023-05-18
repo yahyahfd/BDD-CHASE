48b919a8f16bc9d9db39c64ca44eeec012402a6a
MEZIANE reda <reda.meziane@etu.univ-paris-diderot.fr>
2023-05-18 19:47:13 +0200

    ajout de obliviousChase

--- src/main/java/fr/uparis/algorithms/Standard.java
+++ src/main/java/fr/uparis/algorithms/Standard.java
@@ -72,4 +72,62 @@ public class Standard {
         
         return true;
     }
+
+    public static boolean obliviousChase(Database database/*, long maxExecutionTimeMillis*/) {
+        /*long startTime = System.currentTimeMillis();
+        long endTime = startTime + maxExecutionTimeMillis;*/
+
+        try {
+            /*while(System.currentTimeMillis() < endTime){
+                boolean appliedConstraint = false;*/
+
+            for (GenerationDependencies generationDependency : database.getGenerationDependencies()) {
+                // On vérifie pour chaque tuple de table si generationDependency.corps est
+                // satisfaite
+                // Si c'est le cas, on vérifie si ça satisfait PAS generationDependency.tete
+                // et si e n'a pas été appliquée à ce tuple (? comment ça mon reuf, avec ces
+                // for, c'est deja le cas?)
+
+                if (generationDependency instanceof TGD) {
+                    /*
+                     * ajouter un nouveau tuple u a D
+                     * t.q. D ∪ u satisfait e
+                     */
+                    TGD tgd = (TGD) generationDependency;
+                    Pair<Table, List<Object>> toAdd = tgd.isSatisfiedOblivious();
+
+                    while (toAdd != null) {
+                        List<MutablePair<String, Object>> rowsToAdd = new ArrayList<>();
+                        Table table = toAdd.getLeft();
+                        List<Object> newTuple = toAdd.getRight();
+                        for (String columnName : table.getColumns()) {
+                            int index = table.getColumnIndex(columnName);
+                            rowsToAdd.add(MutablePair.of(columnName,newTuple.get(index)));
+                        }
+                        boolean trueorfalse = table.addRow(rowsToAdd, database);
+                        System.out.println("updated TGD? " + trueorfalse);
+
+                        //appliedConstraint = true;
+                        toAdd = tgd.isSatisfied();
+
+                        // on verifie le temps écoulé
+                            /*if(System.currentTimeMillis() >= endTime){
+                                return false; // si le temps est écoulé, fin de l'algo
+                            }*/
+                    }
+                }
+            }
+
+                /*if(!appliedConstraint){
+                    // si aucune contrainte a été appliquée lors du parcours, fin de l'algo
+                    return true;
+                }
+            }*/
+        } catch (Exception e) {
+            System.out.println("exception : "+e);
+            return false;
+        }
+        // renvoyer false si D non satisfaite si on arrete le programme? je ne sais pas
+        return true;
+    }
 }
